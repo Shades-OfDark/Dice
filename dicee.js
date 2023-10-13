09c3733ad6b75447eb1e6618208c0175709acc27
Mayank <96480121+Shades-OfDark@users.noreply.github.com>
2023-10-14 00:31:01 +0530

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -1,10 +1,10 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
-var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
 document.querySelectorAll(".img1")[0].setAttribute("src", randomDiceImage1);
 
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll(".img2")[0].setAttribute("src", randomDiceImage2);
 
  if(randomNumber1 > randomNumber2)
